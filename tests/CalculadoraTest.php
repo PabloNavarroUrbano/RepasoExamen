b320d1f37496adf1157505a7c965410cacbc1868
Pablo Navarro <navarrourbanopablo@gmail.com>
2024-02-04 15:53:42 +0100

    Subiendo valores iniciales

---
+++ tests/CalculadoraTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+include './src/Calculadora.php';
+
+final class CalculadoraTest extends TestCase
+{
+    public function testSuma()
+    {
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(8, $calc->suma());
+    }
+
+    public function testMult()
+    {
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(15, $calc->mult());    
+    }
+
+    public function testDiv()
+    {
+        $calc = new Calculadora(4,2);
+        $this->assertEquals(2, $calc->div()); 
+        
+    }
+
+}
+?>
